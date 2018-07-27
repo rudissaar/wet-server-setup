a224a32bb74aca79f45501f6a9a91dcaa4d4b09c
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-07-27 19:52:32 +0000

    Improved install-common-maps.sh script.

--- setup/common/install-common-maps.sh
+++ setup/common/install-common-maps.sh
@@ -8,12 +8,29 @@ if [[ "${UID}" != '0' ]]; then
     exit 1
 fi
 
+if [[ ! -d "${WET_DIR}" ]] || [[ -d "${WET_DIR}/etmain" ]]; then
+    echo "> Destination directory doesn't exist, terminating."
+    exit 1
+fi
+
 MAPS=( \
     'adlernest.pk3' \
+    'braundorf_b4.pk3' \
+    'bremen_b2.pk3' \
+    'caen2.pk3' \
+    'et_beach.pk3' \
+    'et_ice.pk3' \
+    'frostbite.pk3' \
+    'karsiah_te2.pk3' \
+    'missile_b3.pk3' \
+    'reactor_final.pk3' \
     'sp_delivery_te.pk3' \
     'supply.pk3' \
+    'sw_battery.pk3' \
     'sw_goldrush_te.pk3' \
+    'tc_base.pk3' \
     'te_valhalla.pk3' \
+    'tournementdm2.pk3' \
 )
 
 for MAP in ${MAPS[@]}
@@ -28,3 +45,4 @@ chown -R 'wet:wet' "${WET_DIR}"
 chmod -R o-rwx "${WET_DIR}"
 
 echo '> Finished.'
+
